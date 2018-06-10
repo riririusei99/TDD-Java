24c5f6a9e16be316b1391435f93e810a1efbd2f1
Ryusei Namai <riry1313@gmail.com>
2018-06-10 18:01:09 +0900

    First Commit(Chapter5)

---
+++ src/money/MoneyTest.java
@@ -0,0 +1,24 @@
+package money;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+	@Test
+	public void testFrancMultiplication() {
+		assertTrue(new Franc(5).equals(new Franc(5)));
+		assertFalse(new Franc(5).equals(new Franc(6)));
+	}
+}
